663fe74c0a2550187cde58fd5470e1ca48064743
vemanisha <manishavermaa786@gmail.com>
2019-05-17 02:17:57 -0400

    123

--- src/ca/sheridancollege/week1/manisha/Student.java
+++ src/ca/sheridancollege/week1/manisha/Student.java
@@ -10,5 +10,21 @@ package ca.sheridancollege.week1.manisha;
  * @author manis
  */
 public class Student {
+    private String name;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     
 }
